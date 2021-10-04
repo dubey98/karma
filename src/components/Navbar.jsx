fff92546f522d01d2b1bd6b5c625cd0a6d722184
Shiv Dubey <shivdubey.dev@gmail.com>
2021-10-05 04:14:59 +0530

    made navbar to host profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,13 @@
 import React, { useRef } from "react";
-import { useAuth } from "./../services/useAuth";
 import { Link } from "react-router-dom";
+import NavbarUserDropDown from "./NavbarUserDropDown";
 
 const Navbar = () => {
-  const auth = useAuth();
   const burgerMenu = useRef(null);
-  const navbarMain = useRef(null);
-  const profileDropdown = useRef(null);
 
   function toggleBurgerMenu() {
-    if (burgerMenu !== null && navbarMain !== null) {
+    if (burgerMenu !== null) {
       burgerMenu.current.classList.toggle("is-active");
-      navbarMain.current.classList.toggle("is-active");
     }
   }
 
@@ -23,14 +19,8 @@ const Navbar = () => {
     >
       <div className="container">
         <div className="navbar-brand">
-          <Link className="navbar-item" to="/">
-            <span className="is-size-4 has-text-weight-semibold ">
-              <strong>KARMA</strong>
-            </span>
-          </Link>
-
           <button
-            className="navbar-burger has-text-weight-bold"
+            className="burger-left has-text-weight-bold"
             aria-label="menu"
             aria-expanded="false"
             onClick={() => toggleBurgerMenu()}
@@ -41,80 +31,17 @@ const Navbar = () => {
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
           </button>
-        </div>
 
-        <div id="navbarMain" className="navbar-menu" ref={navbarMain}>
-          <div className="navbar-end">
-            <div className="navbar-item">
-              {auth.user ? (
-                <div className="buttons">
-                  <div
-                    className="navbar-item has-dropdown is-hoverable"
-                    ref={profileDropdown}
-                  >
-                    <Link to="/" className="navbar-link is-arrowless">
-                      {auth.user.photoURL ? (
-                        <figure className="image is-32x32">
-                          <img
-                            className="is-rounded"
-                            src={auth.user.photoURL}
-                            alt="user profile"
-                          />
-                          {console.log(auth.user.photoURL)}
-                        </figure>
-                      ) : (
-                        <span className="icon">
-                          <i className="far fa-user"></i>
-                        </span>
-                      )}
-                    </Link>
-
-                    <div className="navbar-dropdown is-right">
-                      <Link to="/" className="navbar-item">
-                        Profile
-                      </Link>
-                      <Link to="/" className="navbar-item">
-                        Settings
-                      </Link>
-                      <hr className="navbar-divider" />
-                      <Link
-                        to="/"
-                        className="navbar-item is-selectable"
-                        onClick={async () => {
-                          await auth.signOut();
-                          toggleBurgerMenu();
-                        }}
-                      >
-                        Log Out
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              ) : (
-                <div className="buttons">
-                  <button
-                    className="button has-background-success-light"
-                    onClick={async () => {
-                      await auth.signIn();
-                      toggleBurgerMenu();
-                    }}
-                  >
-                    <strong>Sign up</strong>
-                  </button>
-                  <button
-                    className="button has-background-success-light"
-                    onClick={async () => {
-                      await auth.signIn();
-                      toggleBurgerMenu();
-                    }}
-                  >
-                    Log in
-                  </button>
-                </div>
-              )}
-            </div>
-          </div>
+          <Link className="navbar-item" to="/">
+            <span className="is-size-4 has-text-weight-semibold ">
+              <strong>KARMA</strong>
+            </span>
+          </Link>
+          <NavbarUserDropDown />
         </div>
+        {/* <div id="navbarMain" className="navbar-menu">
+          <div className="navbar-end"></div>
+        </div> */}
       </div>
     </nav>
   );
---
+++ src/components/NavbarUserDropDown.jsx
@@ -0,0 +1,104 @@
+import React, { useRef } from "react";
+import { useAuth } from "../services/useAuth";
+import { Link } from "react-router-dom";
+
+const NavbarUserDropDown = () => {
+  const auth = useAuth();
+  const profileDropdown = useRef(null);
+  const profileDropDownIcon = useRef(null);
+
+  function toggleMenu() {
+    if (profileDropDownIcon !== null) {
+      profileDropDownIcon.current.classList.toggle("is-active");
+    }
+  }
+
+  function handleMouseEnter() {
+    if (profileDropdown !== null) {
+      profileDropdown.current.classList.add("is-block");
+    }
+  }
+
+  function handleMouseLeave() {
+    if (profileDropdown !== null) {
+      profileDropdown.current.classList.remove("is-block");
+    }
+  }
+
+  return (
+    <div className="navbar-item burger-right">
+      {auth.user ? (
+        <div className="buttons">
+          <div
+            className="navbar-item has-dropdown"
+            ref={profileDropDownIcon}
+            onMouseEnter={() => handleMouseEnter()}
+            onMouseLeave={() => handleMouseLeave()}
+          >
+            <Link to="/" className="navbar-link is-arrowless">
+              {auth.user.photoURL ? (
+                <figure className="image is-32x32">
+                  <img
+                    className="is-rounded"
+                    src={auth.user.photoURL}
+                    alt="user profile"
+                  />
+                </figure>
+              ) : (
+                <span className="icon">
+                  <i className="far fa-user"></i>
+                </span>
+              )}
+            </Link>
+
+            <div
+              className="navbar-dropdown navbar-dropdown-persist is-right-extended"
+              ref={profileDropdown}
+            >
+              <Link to="/" className="navbar-item">
+                Profile
+              </Link>
+              <Link to="/" className="navbar-item">
+                Settings
+              </Link>
+              <hr className="navbar-divider" />
+              <Link
+                to="/"
+                className="navbar-item is-selectable"
+                onClick={async () => {
+                  await auth.signOut();
+                  toggleMenu();
+                }}
+              >
+                Log Out
+              </Link>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="buttons">
+          <button
+            className="button has-background-success-light"
+            onClick={async () => {
+              await auth.signIn();
+              toggleMenu();
+            }}
+          >
+            <strong>Sign up</strong>
+          </button>
+          <button
+            className="button has-background-success-light"
+            onClick={async () => {
+              await auth.signIn();
+              toggleMenu();
+            }}
+          >
+            Log in
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NavbarUserDropDown;
